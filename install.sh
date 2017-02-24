fb6d14d278aa23ad5f46b8b927b20413cb4c32ff
Massimiliano Cuttini <massimiliano.cuttini@phoenixweb.it>
2017-02-24 20:47:13 +0100

    fix to installer for XEN7.0

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ cp repos/ceph-$CEPH_VERSION.repo /etc/yum.repos.d/ceph-$CEPH_VERSION.repo
 echo "Install the release.asc key"
 rpm --import 'https://download.ceph.com/keys/release.asc'
 
-echo "Enable Repo"
+echo "Fix Repo Path and Enable"
 sed -ie 's/\$releasever/7/g' /etc/yum.repos.d/CentOS-Base.repo
 sed -ie '/\[base\]/,/^\[/s/enabled=0/enabled=1/' /etc/yum.repos.d/CentOS-Base.repo
 sed -ie '/\[updates\]/,/^\[/s/enabled=0/enabled=1/' /etc/yum.repos.d/CentOS-Base.repo
@@ -20,17 +20,25 @@ echo "Install Required Packages"
 yum install -y epel-release
 yum install -y yum-plugin-priorities.noarch
 yum install -y snappy leveldb gdisk python-argparse gperftools-libs
-yum install -y fuse fuse-libs
-yum install -y ceph-common rbd-fuse rbd-nbd
+#yum install -y fuse fuse-libs #skip due to conflict with XenServer
+yum install -y ceph-common rbd-nbd
 
 echo "Disable Repos"
-sed -ie '/\[base\]/,/^\[/s/enabled=1/enabled=0/' /etc/yum.repos.d/CentOS-Base.repo
-sed -ie '/\[updates\]/,/^\[/s/enabled=1/enabled=0/' /etc/yum.repos.d/CentOS-Base.repo
-sed -ie '/\[extras\]/,/^\[/s/enabled=1/enabled=0/' /etc/yum.repos.d/CentOS-Base.repo
-sed -ie '/\[epel\]/,/^\[/s/enabled=1/enabled=0/' /etc/yum.repos.d/epel.repo
+sed -ie 's/enabled=1/enabled=0/g' /etc/yum.repos.d/CentOS-Base.repo
+sed -ie 's/enabled=1/enabled=0/g' /etc/yum.repos.d/epel.repo
 
 echo "Installing files"
 cp bins/waitdmmerging.sh		/usr/bin
 chmod 755 /usr/bin/waitdmmerging.sh
-cp bins/ceph_plugin.py			/etc/xapi.d/plugins/ceph_plugin
-cp bins/cephutils.py RBDSR.py	/opt/xensource/sm
+cp bins/ceph_plugin.py	/etc/xapi.d/plugins/ceph_plugin
+chmod +x /etc/xapi.d/plugins/ceph_plugin
+
+cp bins/RBDSR.py		/opt/xensource/sm
+cp bins/cephutils.py	/opt/xensource/sm
+python -m compileall /opt/xensource/sm/RBDSR.py
+python -O -m compileall /opt/xensource/sm/RBDSR.py
+python -m compileall /opt/xensource/sm/cephutils.py
+python -O -m compileall /opt/xensource/sm/cephutils.py
+chmod +x /opt/xensource/sm/RBDSR.py
+chmod +x /opt/xensource/sm/cephutils.py
+ln -s RBDSR.py RBDSR
