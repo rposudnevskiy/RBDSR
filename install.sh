9d896a97738b6b459f37939445ffab19f6b63ddb
Massimiliano Cuttini <massimiliano.cuttini@phoenixweb.it>
2017-02-25 16:37:27 +0100

    fix: enable and disable properly all the Repos

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo "Install RBDSR plugin"
 echo "****************"
 
 echo "Install new Repos"
-cp repos/ceph-$CEPH_VERSION.repo /etc/yum.repos.d/ceph-$CEPH_VERSION.repo
+cp -n repos/ceph-$CEPH_VERSION.repo /etc/yum.repos.d/
 
 echo "Install the release.asc key"
 rpm --import 'https://download.ceph.com/keys/release.asc'
@@ -19,6 +19,8 @@ sed -ie '/\[extras\]/,/^\[/s/enabled=0/enabled=1/' /etc/yum.repos.d/CentOS-Base.
 echo "Install Required Packages"
 yum install -y epel-release
 yum install -y yum-plugin-priorities.noarch
+
+sed -ie 's/enabled=0/enabled=1/g' /etc/yum.repos.d/epel.repo
 yum install -y snappy leveldb gdisk python-argparse gperftools-libs
 yum install -y fuse fuse-libs
 yum install ceph-common rbd-fuse rbd-nbd
