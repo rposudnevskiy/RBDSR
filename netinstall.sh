08303cff6d651fea9a7bb877a12634da2aeeaf19
Massimiliano Cuttini <massimiliano.cuttini@phoenixweb.it>
2017-02-25 16:24:44 +0100

    fix: fix relative install path

--- netinstall.sh
+++ netinstall.sh
@@ -10,5 +10,6 @@ cd ~
 wget "$REPO/$PROJECT/archive/$BRANCH.zip" -O ~/$PROJECT-temp.zip
 unzip ~/$PROJECT-temp.zip -d ~
 mv ~/$PROJECT-$BRANCH/ ~/$PROJECT/
-sh ~/$PROJECT/install.sh
+cd ~/$PROJECT/
+sh ./install.sh
 rm -rf ~/$PROJECT-temp.zip
