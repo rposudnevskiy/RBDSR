ca787ecf75a157ca96b306089ee67d9b382305f6
Roman Posudnevskiy <ramzes_r@yahoo.com>
2017-11-13 23:06:25 +0400

    Fix for the issue with branch name mentioned in #59

--- netinstall.sh
+++ netinstall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 REPO="https://github.com/rposudnevskiy"
 PROJECT="RBDSR"
-BRANCH="v2.0"
+BRANCH="2.0"
 cd ~
-wget "$REPO/$PROJECT/archive/$BRANCH.zip" -O ~/$PROJECT-temp.zip
+wget "$REPO/$PROJECT/archive/v$BRANCH.zip" -O ~/$PROJECT-temp.zip
 unzip ~/$PROJECT-temp.zip -d ~
 mv ~/$PROJECT-$BRANCH/ ~/$PROJECT/
 cd ~/$PROJECT/
