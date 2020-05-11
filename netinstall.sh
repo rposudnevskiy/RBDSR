717ee640d08d9653fd03b9e5f52b6a920db6f8bf
Roman Posudnevskiy <ramzes_r@yahoo.com>
2020-05-11 20:17:42 +0300

    Fixed wget invocation and branch name

--- netinstall.sh
+++ netinstall.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 REPO="https://github.com/rposudnevskiy"
 PROJECT="RBDSR"
-BRANCH="3.0"
+BRANCH="v3.0"
 
 #Install xcpng-storage-libs
 sh <(curl -s https://raw.githubusercontent.com/rposudnevskiy/xcpng-storage-libs/master/netinstall.sh)
 
 cd ~
-wget "$REPO/$PROJECT/archive/v$BRANCH.zip" -O ~/$PROJECT-temp.zip
+wget "$REPO/$PROJECT/archive/$BRANCH.zip" -O ~/$PROJECT-temp.zip
 unzip ~/$PROJECT-temp.zip -d ~
 cd ~/$PROJECT-$BRANCH/install
 
